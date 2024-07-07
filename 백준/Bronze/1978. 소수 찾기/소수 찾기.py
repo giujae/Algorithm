a66ec3683eb5870acd0a83de89cdb383024cee58
정재욱 <122589766+giujae@users.noreply.github.com>
2024-07-07 14:19:06 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,19 +1,30 @@
 N = int(input())
 numbers = list(map(int, input().split()))
 
-prime = [2, 3]
-
-for num in range(5, 1001, 2):
-    for p_num in prime:
-        if p_num * p_num <= num:
-            if num % p_num == 0:
-                break
+def find_prime(numbers):
+    cnt = 0
+    prime = [2, 3]
+    if max(numbers) == 1:
+        return 0
+    elif max(numbers) == 2:
+        return 1
+    elif max(numbers) == 3:
+        if 2 in numbers:
+            return 2
+        else:
+            return 1
     else:
-        prime.append(num)
+        for num in range(5, max(numbers) + 1, 2):
+            for p_num in prime:
+                if p_num * p_num <= num:
+                    if num % p_num == 0:
+                        break
+            else:
+                prime.append(num)
+                
+    for number in numbers:
+        if number in prime:
+            cnt += 1
+    return cnt
 
-cnt = 0
-
-for number in numbers:
-    if number in prime:
-        cnt += 1
-print(cnt)
+print(find_prime(numbers))
