bc82bb86545371b9dd347597dfe52a489c537eb5
정재욱 <122589766+giujae@users.noreply.github.com>
2024-07-11 11:12:01 +0900

    [Silver I] Title: 쿼드트리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -3,22 +3,28 @@ N = int(input())
 arr = [list(map(int, input())) for _ in range(N)]
 
 def compressor(N, arr):
-    sum_lst = 0
-    
+    # 영상을 합산 할 변수
+    temp = 0
+    # 영상의 합
     for lst in arr:
-        sum_lst += sum(lst)
-
-    if sum_lst == N ** 2:
+        temp += sum(lst)
+    # 영상이 모두 1로만 되어 있으면
+    if temp == N ** 2:
         return '1'
-    
-    if sum_lst == 0:
+    # 영상이 모드 0으로만 되어 있으면
+    if temp == 0:
         return '0'
-    
-    result = '('
-    result += compressor(N//2, [h_arr[:N//2] for h_arr in arr[:N//2]])
-    result += compressor(N//2, [h_arr[N//2:] for h_arr in arr[:N//2]])
-    result += compressor(N//2, [h_arr[:N//2] for h_arr in arr[N//2:]])
-    result += compressor(N//2, [h_arr[N//2:] for h_arr in arr[N//2:]])
+    # 0과 1이 섞여있는 경우
+    # 결과는 괄호안에 묶어서
+    result = '(' 
+    # 왼쪽 위
+    result += compressor(N//2, [elem[:N//2] for elem in arr[:N//2]])
+    # 오른쪽 위
+    result += compressor(N//2, [elem[N//2:] for elem in arr[:N//2]])
+    # 왼쪽 아래
+    result += compressor(N//2, [elem[:N//2] for elem in arr[N//2:]])
+    # 오른쪽 아래   
+    result += compressor(N//2, [elem[N//2:] for elem in arr[N//2:]])
     result += ')'
     
     return result
