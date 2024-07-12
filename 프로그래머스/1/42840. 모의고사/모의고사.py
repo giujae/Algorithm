0ab6d4ec17aa89c91465983e8093862760c1f9b7
정재욱 <122589766+giujae@users.noreply.github.com>
2024-07-12 11:29:36 +0900

    [level 1] Title: 모의고사, Time: 7.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,19 +1,21 @@
 def solution(answers):
-    scores = []
+    sub_ans1 = [1, 2, 3, 4, 5]
+    sub_ans2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    sub_ans3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    scores = [0] * 3
+    
     result = []
-    sub_ans1 = [1, 2, 3, 4, 5] * (len(answers) // 5) + [1, 2, 3, 4, 5][:(len(answers) % 5)]
-    sub_ans2 = [2, 1, 2, 3, 2, 4, 2, 5] * (len(answers) // 8) + [2, 1, 2, 3, 2, 4, 2, 5][:(len(answers) % 8)]
-    sub_ans3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * (len(answers) // 10) + [3, 3, 1, 1, 2, 2, 4, 4, 5, 5][:(len(answers) % 10)]
-
-    for sub in [sub_ans1, sub_ans2, sub_ans3]:
-        cnt = 0
-        for i in range(len(sub)):
-            if sub[i] == answers[i]:
-                cnt += 1
-            else:
-                pass
-        scores.append(cnt)
-    for i in range(len(scores)):
-        if scores[i] == max(scores):
-            result.append(i+1)
+    
+    for i in range(len(answers)):
+        if answers[i] == sub_ans1[i % len(sub_ans1)]:
+            scores[0] += 1
+        if answers[i] == sub_ans2[i % len(sub_ans2)]:
+            scores[1] += 1
+        if answers[i] == sub_ans3[i % len(sub_ans3)]:
+            scores[2] += 1
+    
+    for idx, score in enumerate(scores):
+        if score == max(scores):
+            result.append(idx + 1)
     return result
