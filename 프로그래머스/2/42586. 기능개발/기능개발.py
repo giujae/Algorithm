052c3edc87aa5aae2380e38f800131755daf823c
정재욱 <122589766+giujae@users.noreply.github.com>
2024-07-13 22:45:08 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,3 +1,18 @@
+from collections import deque
+
 def solution(progresses, speeds):
+    queue = deque([work for work in zip(progresses, speeds)])
     answer = []
+    cnt = 0
+    date = 0
+    while len(queue) > 0:
+        if (queue[0][0] + date * queue[0][1]) >= 100:
+            queue.popleft()
+            cnt += 1
+        else:
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+            date += 1
+    answer.append(cnt)
     return answer
