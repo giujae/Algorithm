57d64bb5a47a5c5ac5c80b825a67f3fead623322
정재욱 <122589766+giujae@users.noreply.github.com>
2024-07-17 15:57:35 +0900

    [level 2] Title: 타겟 넘버, Time: 331.57 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,8 @@
+from itertools import product
+
+
 def solution(numbers, target):
-    answer = 0
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
 
-    def find_sum(idx, current_sum):
-        nonlocal answer
-
-        if idx == len(numbers):
-            if current_sum == target:
-                answer += 1
-            return
-
-        find_sum(idx + 1, current_sum + numbers[idx])
-        find_sum(idx + 1, current_sum - numbers[idx])
-
-    find_sum(0, 0)
-    return answer
+    return s.count(target)
