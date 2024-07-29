c5f3cd8fc448198581c3bac3acb9239d33ebe21b
정재욱 <122589766+giujae@users.noreply.github.com>
2024-07-29 10:35:04 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,13 @@
+N = int(input())
+result = -1
+
+for i in range(N // 5, -1, -1):
+    if (N - 5 * i) == 0:
+        result = i
+        break
+    else:
+        if (N - 5 * i) % 3 == 0:
+            result = i + (N - 5 * i) // 3
+            break
+
+print(result)
