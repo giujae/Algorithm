c1d64e7067078e429a85777c14fcf64797948214
정재욱 <122589766+giujae@users.noreply.github.com>
2024-07-29 10:55:41 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,13 +1,14 @@
 N = int(input())
-result = -1
+dp = [-1] * 5001
 
-for i in range(N // 5, -1, -1):
-    if (N - 5 * i) == 0:
-        result = i
-        break
-    else:
-        if (N - 5 * i) % 3 == 0:
-            result = i + (N - 5 * i) // 3
-            break
+dp[3] = dp[5] = 1
 
-print(result)
+for i in range(6, N + 1):
+    if not i % 5:
+        dp[i] = dp[i - 5] + 1
+    elif not i % 3:
+        dp[i] = dp[i - 3] + 1
+    elif dp[i - 3] > 0 and dp[i - 5] > 0:
+        dp[i] = min(dp[i - 3], dp[i - 5]) + 1
+
+print(dp[N])
