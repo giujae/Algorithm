f90d3311d151167817e5f4feaa746cc2f338497d
정재욱 <122589766+giujae@users.noreply.github.com>
2024-08-10 10:35:23 +0900

    [Gold V] Title: 하노이 탑, Time: 764 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -1,17 +1,20 @@
 N = int(input())
 
+
 def move(num, x, y):
 
-    if(N <= 20):
+    if N <= 20:
         if num == 1:
             print(x, y)
             return
-        
-        move(num-1, x, 6-x-y)
-            
+
+        move(num - 1, x, 6 - x - y)
+
         print(x, y)
 
-        move(num-1, 6-x-y, y)
+        move(num - 1, 6 - x - y, y)
 
-print(2**N -1)
+
+print(2**N - 1)
+
 move(N, 1, 3)
