10c4aa3869d3956e5a0e74eb1d4046549479513f
정재욱 <122589766+giujae@users.noreply.github.com>
2025-05-25 11:42:59 +0900

    [level 2] Title: H-Index, Time: 0.51 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,10 +1,10 @@
 function solution(citations) {
-    let answer = 0;
     citations.sort((a,b) => (b-a));
-    for(let i=0; i<citations.length; i++){
-        if(citations[i] < i+1) {
-            return answer = i
-        }
+    
+    let i = 0;
+    
+    while(citations[i] >= i+1){
+        i++
     }
-    return citations.length
+    return i
 }
